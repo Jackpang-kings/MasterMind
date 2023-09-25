9d29d32ec2d9a596005929f89fc04eaffa698372
Jack Pang <144328990+Jackpang-kings@users.noreply.github.com>
2023-09-25 14:07:51 +0000

    Update1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
+using System.Xml;
 namespace HelloWorld { 
 class Program { 
-static void Main(){ 
+    string UserInput;
+    static void Main(){ 
+    int Turns = 0;
+    //Generating the correct answer
+    string[]CorrectAns = GenAns();
+    while (Turns < 12){
+        InputAns();
+        ProcessAns();
 
-} 
+    }
+}
+
+static string[] InputAns(){
+    string[] ans = new string[3];
+    for (int i = 0, i > 3; i++){
+        Console.Write($"Enter the {i+1} guess");
+        ans[i] = Console.ReadLine();
+    }
+    return ans;
+}
+
+static string[] GenAns(){
+    string[] CorrectAns = new string[3];
+    return CorrectAns;
+}
+
+static string[] ProcessAns(){
+    
+    return;
+}
+
+static string CurrentPos(){
+    return;
+}
 } 
 }
 
