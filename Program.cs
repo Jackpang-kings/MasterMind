3d6659d85fed54029502456abe34df30c7ac927d
Jackpang-kings <jpang18@bi.kgapupils.uk>
2023-09-26 16:24:40 +0100

    Update4

--- Program.cs
+++ Program.cs
@@ -4,74 +4,86 @@ using System.Xml;
 namespace HelloWorld { 
 class Program { 
     static void Main(){ 
-    (string[] ColourInput, string[] hints)[]UserInput = new (string[], string[])[12];
-    int Turns = 1;
+    (string ColourInput, string hints)[]UserInput = new (string, string)[12];   
+    int Turns = 0;
     //Generating the correct answer
     string[]CorrectAns = GenAns();
-    while (Turns < UserInput.Length){
-        string[] colours = UserInput[Turns].ColourInput;
-        string[] hints = UserInput[Turns].hints;
-        colours = InputAns();
-        CurrentPos(UserInput);
-        hints = ProcessAns(UserInput,CorrectAns,Turns);
-        Console.WriteLine(Match(hints));
+    PrintGen(CorrectAns);
+    while (Turns < 12){
+        UserInput[Turns].ColourInput = InputAns();
+        string colours = UserInput[Turns].ColourInput;
+        PrintInt(UserInput,colours,Turns);
+        UserInput[Turns].hints = ProcessAns(UserInput,CorrectAns,Turns);
+        string hints = UserInput[Turns].hints;
+        CurrentPos(UserInput,hints,colours);
+        if (hints == "WIN"){
+            break;
+        }
         Turns++;
     }
 }
 
-static string[] InputAns(){
-    string[] ans = new string[4];
-    for (int i = 0; i < 4; i++){
-        Console.Write($"Enter the {i+1} guess");
-        ans[i] = Console.ReadLine();
+static void PrintGen(string[] CorrectAns){
+    for (int i = 0; i < CorrectAns.Length; i++){
+        Console.Write($"{i+1}" + "|" + CorrectAns[i] + "|");
+        Console.WriteLine("");
     }
+}
+
+static void PrintInt((string ColourInput, string hints)[]UserInput, string Colour,int Turns){
+    Console.Write($"{Turns+1}" + "|" + Colour + "|");
+    Console.WriteLine("");
+}
+static string InputAns(){
+    Console.WriteLine("Enter a guess: (eg:red,yellow,blue,green):");
+    string ans = Console.ReadLine();
     return ans;
 }
 
 static string[] GenAns(){
     string[] Colour = {"red","yellow","blue","green","purple","orange","black","white"};
-    string[] CorrectAns = new string[3];
+    string[] CorrectAns = new string[4];
     Random r = new Random();
-    int i = r.Next(7);
-    for (int n = 0; n < 3; n++){
+    for (int n = 0; n < CorrectAns.Length; n++){
+        int i = r.Next(7);
         CorrectAns[n] = Colour[i];
     }
     return CorrectAns;
 }
 
-static string[] ProcessAns((string[] ColourInput, string[] hints)[]UserInput, string[] CorrectAns,int Turns){
-    int Match = 0;
-    string[] colour = UserInput[Turns].ColourInput;
-    string[] hints = UserInput[Turns].hints;
-    
-    for (int i = 0; i < 3;i++){
-        if (colour[i] == CorrectAns[i]){
+static string ProcessAns((string ColourInput, string hints)[] UserInput, string[] CorrectAns, int Turns){
+    string[] hints = new string[4];
+    string[] ciarray = UserInput[Turns].ColourInput.Split(",");
+    int whitePegsCount = 0;
+    int redPegsCount = 0;
+    for (int i = 0; i < 4; i++){
+        if (ciarray[i] == CorrectAns[i]){
             hints[i] = "white";
-        }else if (CorrectAns.Contains(colour[i])){
+            whitePegsCount++;
+        }
+        else if (Array.Exists(CorrectAns, element => element == ciarray[i])){
             hints[i] = "red";
+            redPegsCount++;
         }else{
             hints[i] = "nothing";
         }
     }
-    return hints;
+    string jointhint;
+    Console.WriteLine("White Pegs: " + whitePegsCount);
+    Console.WriteLine("Red Pegs: " + redPegsCount);
+    if (whitePegsCount == 4){
+        Console.WriteLine("Congratulations!");
+        jointhint = "WIN";
+    }else{
+        Console.WriteLine("Wrong guess");
+        jointhint = string.Join(",",hints);
     }
-
-static void CurrentPos((string[] ColourInput, string[] hints)[]UserInput){
-    for (int i = 0; i < UserInput.Length; i++){
-        Console.Write($"{i+1}");
-        for (int j = 0; j < hints.Length;j++){
-            Console.Write("|" + hints[j] + "|");
-        }
-    }
+    return jointhint;
 }
 
-static string Match(string[] hints){
-    if (hints[0] == "white" & hints[1] == "white" & hints[2] == "white" & hints[3] == "white"){
-            return "Wrong";
-        }else{
-            return "Correct";
-        }
+static void CurrentPos((string ColourInput, string hints)[]UserInput, string hints, string Colour){
+    Console.WriteLine("|" + Colour + "|");
+    Console.WriteLine("|" + hints + "|");
 }
-} 
 }
-
+}
