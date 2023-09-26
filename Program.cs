23e5b8c3e4a0e6b0979a6607633917b5d220d2fc
Jack Pang <144328990+Jackpang-kings@users.noreply.github.com>
2023-09-26 12:35:41 +0000

    Update2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Xml;
 namespace HelloWorld { 
 class Program { 
-    string UserInput;
     static void Main(){ 
-    int Turns = 0;
+    (string[] ColourInput, string[] hints)[]UserInput = new (string[], string[])[11];
+    int Turns = 1;
     //Generating the correct answer
     string[]CorrectAns = GenAns();
-    while (Turns < 12){
-        InputAns();
-        ProcessAns();
-
+    while (Turns < 13){
+        string[] colours = UserInput[Turns].ColourInput;
+        string[] hints = UserInput[Turns].hints;
+        colours = InputAns();
+        //CurrentPos(UserInput,Turns);
+        hints = ProcessAns(UserInput,CorrectAns,Turns);
+        Console.WriteLine(Match(hints));
+        Turns++;
     }
 }
 
 static string[] InputAns(){
     string[] ans = new string[3];
-    for (int i = 0, i > 3; i++){
+    for (int i = 0; i < 3; i++){
         Console.Write($"Enter the {i+1} guess");
         ans[i] = Console.ReadLine();
     }
@@ -24,17 +29,46 @@ static string[] InputAns(){
 }
 
 static string[] GenAns(){
+    string[] Colour = {"red","yellow","blue","green","purple","orange","black","white"};
     string[] CorrectAns = new string[3];
+    Random r = new Random();
+    int i = r.Next(7);
+    for (int n = 0; n < 3; n++){
+        CorrectAns[n] = Colour[i];
+    }
     return CorrectAns;
 }
 
-static string[] ProcessAns(){
+static string[] ProcessAns((string[] ColourInput, string[] hints)[]UserInput, string[] CorrectAns,int Turns){
+    int Match = 0;
+    string[] colour = UserInput[Turns].ColourInput;
+    string[] hints = UserInput[Turns].hints;
     
+    for (int i = 0; i < 3;i++){
+        if (colour[i] == CorrectAns[i]){
+            hints[i] = "white";
+        }else if (CorrectAns.Contains(colour[i])){
+            hints[i] = "red";
+        }else{
+            hints[i] = "nothing";
+        }
+    }
+    return hints;
+    }
+
+static string CurrentPos((string[] ColourInput, string[] hints)[]UserInput, int Turns){
+    for (int i = 0; i < Turns; i++){
+        
+    }
     return;
 }
 
-static string CurrentPos(){
-    return;
+static string Match(string[] hints){
+    if (hints[0] == "white" & hints[1] == "white" & hints[2] == "white" & hints[3] == "white"){
+            return "Wrong";
+        }else{
+            return "Correct";
+        }
 }
 } 
 }
